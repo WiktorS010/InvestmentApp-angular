16eaf6c0e51e464a3ef2519e04361a1989dd6e26
tomaszfranczak <tomaszfk2@gmail.com>
2023-11-07 22:03:01 +0100

    First review.

--- src/main/java/pl/stepien/investmentappangular/coingeckoApiConnection/CoingeckoApiConnection.java
+++ src/main/java/pl/stepien/investmentappangular/coingeckoApiConnection/CoingeckoApiConnection.java
@@ -11,12 +11,24 @@ import pl.stepien.investmentappangular.model.CryptoCurrency;
 
 import java.util.List;
 
+//Sugeruje zrobic z tej klasy service -> CoingeckoService
 @Component
 public class CoingeckoApiConnection {
 
     private final RestTemplate restTemplate = new RestTemplate();
+
+    //dodaj static i wynies ponad wszystkie pola. Trzymanie stalych czyli pol static final na samej gorze to dobra praktyka.
     private final String CRYPTO_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20";
 
+
+    //Tutaj kilka rzecz:
+    // 1. Mamy dwie metody obie korzystaja poki co dokladnie z tego samego - sugeruje zatem wydzielic do oddzielej prawtnej funkcji
+    //    wyciagniecie danych z coingeco. Zaznacz sobie kursorem od znaku rownosci do }); wlacznie i kliknij ctrl + alt + m
+    //    Tak wygenerowana metode mozesz uzyc w obu metodach pubilicznych.
+    // 2. Nie wiem jak ma dokladnie dzialac metoda z parametrem - czy zapytanie do coingecko bedzie parametryzowane czy tez bedziesz
+    //    przetwarzal pobrane dane z uzyciem obiektu of.
+    // 3. Zauwaz ze Coingecko moze zwrocic Ci roznego rodzaju odpowiedzi - niekonicznie zawsze 200 ok, takze zanim pobierzem body i
+    //    zwrocisz odpowiedz dalej - zrob jakas validacje - zaloguj rodzaj odpowiedzi i zwroc np pusta liste.
     public List<CryptoCurrency> getCrypto(PageRequest of) {
         ResponseEntity<List<CryptoCurrency>> response =
                 restTemplate.exchange(CRYPTO_URL, HttpMethod.GET, null,
@@ -24,6 +36,8 @@ public class CoingeckoApiConnection {
                         });
         return response.getBody();
     }
+
+
     public List<CryptoCurrency> getCrypto() {
         ResponseEntity<List<CryptoCurrency>> response =
                 restTemplate.exchange(CRYPTO_URL, HttpMethod.GET, null,
--- src/main/java/pl/stepien/investmentappangular/controller/InvestmentController.java
+++ src/main/java/pl/stepien/investmentappangular/controller/InvestmentController.java
@@ -21,30 +21,41 @@ import java.util.Map;
 @RequestMapping("/investment")
 @RequiredArgsConstructor
 public class InvestmentController {
+
+    //Tutaj moglbys zastosowac wzorzec projektowy Facade/Fasada - poczytaj co to za wzorzec lub obejrzyj jakis filmik
+    //Wtedy idealnie mialbys tu jeden service - fasadowy - ktory wewnwtrz ma wszystkie te inne serwisy
+    //kontroller jest prosty i przejrzysty -> a cala logika biznesowa schowana jest w innej klasie
+
     private final InvestmentService investmentService;
     private final UserService userService;
     private final CoingeckoApiConnection coingeckoApiConnection;
     private final CryptoCurrencyService cryptoCurrencyService;
     private ObjectMapper objectMapper;
 
+    //Podobna sytuacja, nalezy wziac pod uwage rozne sytuacje - nie zawsze ok
     @GetMapping("/list")
     public ResponseEntity<List<Investment>> getSortedInvestments() {
+        //na to co w nawiasie moglbys miec metode w fasadzie
         return ResponseEntity.ok(investmentService.getAllInvestmentsSortedByIncome(userService.getUserById(1L)));
     }
 
+    //Dlaczego to jest save? metoda na podstawie argumentu zwraca obieky cryptoCurrency. Where jakis zapis?
     @GetMapping("/save")
     public ResponseEntity<CryptoCurrency> saveInvestmentPage(HttpSession session) {
         String symbol = (String) session.getAttribute("symbol");
 
+        //loguje, nie wypisuj
         System.out.println(symbol);
 
+        //warto sprawdzic czy aby lista nie jest pusta
         List<CryptoCurrency> cryptoList = coingeckoApiConnection.getCrypto();
         CryptoCurrency cryptoCurrency = cryptoCurrencyService.findCryptoBySymbol(cryptoList, symbol.toUpperCase());
         return ResponseEntity.ok(cryptoCurrency);
     }
 
+    //Zrob jakis model requestu i nim sie posluguj
     @PostMapping("/save")
-    public ResponseEntity<Investment> saveInvestment(@RequestBody Map<String, Object> request,HttpSession session) {
+    public ResponseEntity<Investment> saveInvestment(@RequestBody Map<String, Object> request, HttpSession session) {
         objectMapper.findAndRegisterModules();
         String symbol = (String) session.getAttribute("symbol");
         Investment investment = objectMapper.convertValue(request.get("investment"), Investment.class);
@@ -54,11 +65,17 @@ public class InvestmentController {
         System.out.println(cryptoCurrency);
         investment.setEnterPrice(cryptoCurrency.getPrice());
 
+        //Cala ta logika biznesowa najlepiej by bylo zeby trafila do jakiegos serwisu, a nie zalatwiona w kontrollerze
         System.out.println(investment);
         Investment createdInvestment = investmentService.addInvestment(investment);
         return ResponseEntity.ok(createdInvestment);
     }
 
+
+    //Poczytaj/obejrzyj o samym koncepcie REST API - kontroller zawsze powinien dotyczyc konkretnego zasobu - np cars,
+    //dzieki metodom - jestes w stanie pobierac, zmieniac, usuwac dane zasoby. Nie potrzebujemy w sciezce dodawac GET/DELETE
+    //To powinno byc opisane w necie - to moze byc pytanie na rozmowie.
+    //Co wiecej polecam poczytac o HTTP/HTTPS i znac ze 4/5 metod jakie ma ten protokol
     @GetMapping("/get/{id}")
     public ResponseEntity<Investment> getInvestment(@PathVariable("id") Long id) {
         return ResponseEntity.ok(investmentService.get(id));
